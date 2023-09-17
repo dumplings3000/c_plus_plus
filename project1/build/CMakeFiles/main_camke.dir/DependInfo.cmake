4d434164f58a29220fa310e7a66b5d40d1881b52
sam_iscilab621 <sam8899yy.ee12@nycu.edu.tw>
2023-09-17 18:42:06 +0800

    project1 finish

---
+++ project1/build/CMakeFiles/main_camke.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sam/c_plus_plus/main.cpp" "/home/sam/c_plus_plus/build/CMakeFiles/main_camke.dir/main.cpp.o"
+  "/home/sam/c_plus_plus/src/swap.cpp" "/home/sam/c_plus_plus/build/CMakeFiles/main_camke.dir/src/swap.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project1/include/swap.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+
+class swap
+{
+private:
+    /* data */
+    int _a;
+    int _b;
+
+public:
+    swap(int a, int b)
+    {
+        this->_a = a;
+        this->_b = b;
+    }
+    void run();
+    void printInfo();
+};
